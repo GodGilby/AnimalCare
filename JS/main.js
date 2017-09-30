2034ac41877866a75d9842440e1de04a49392cac
GodGilby <gilby_21@hotmail.com>
2017-09-30 15:35:08 -0500

    CONTACTOS PERFECTO

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+var verDatos = function(){
+        if (document.getElementById("parrafovisible").style.display == 'block'){
+            parrafovisible.style.display = "none";
+        }
+        else{
+            parrafovisible.style.display = "block";
+        }
+    }
