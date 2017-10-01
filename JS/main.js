c0706e7cb9c482dd53174f1113fe27fcee803ac5
GodGilby <gilby_21@hotmail.com>
2017-09-30 23:05:39 -0500

    Nuevos cambios

--- JS/main.js
+++ JS/main.js
@@ -6,3 +6,29 @@ var verDatos = function(){
             parrafovisible.style.display = "block";
         }
     }
+/*
+var cambioImagen = function(){
+    //var imagenes = [imagenmov1,imagenmov2];
+    var x = 0;
+    var z = document.getElementById('imagenmov1');
+     var y = document.getElementById('imagenmov2');
+    var i = 0;
+    while(i < 9000){
+
+        if(x < 1000){
+            z.style.display = "block";
+            y.style.display = "none";
+            i ++;
+        }
+        else if(x < 2000){
+            y.style.display = "block";
+            z.style.display = "none";
+            i++;
+        }
+        else{
+            x= 0;
+        }
+        x++;
+    }
+}
+*/
